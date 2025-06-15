0cfc734b2f33adcb774e0726aff390c5f7ecdf40
Owolabi <owolabi1738@gmail.com>
2025-06-15 23:19:44 +0100

    Completed algorithm and tested it for accuracy.

---
+++ main.js
@@ -0,0 +1,54 @@
+function moveKnight(from, toward) {
+    const possibleMoves = [
+        [1, 2],
+        [-1, 2],
+        [1, -2],
+        [-1, -2],
+        [2, 1],
+        [-2, 1],
+        [2, -1],
+        [-2, -1]
+    ];
+
+    function isCoordinateValid(x, y) {
+        return x >= 0 && y >= 0 && x < 8 && y < 8;
+    }
+
+    const list = [];
+    const passedPoints = new Set();
+
+    list.push({ position: from, direction: [from]});
+    passedPoints.add(`${from[0]}, ${from[1]}`);
+
+    while (list.length > 0) {
+        const presentPosition = list.shift();
+        const [x,y] = presentPosition.position;
+
+        if (x === toward[0] && y === toward[1]) {
+            console.log('Found the shortest path');
+
+            for (let point of presentPosition.direction) {
+                console.log(point);
+            }
+            return presentPosition.direction;
+        }
+
+        for (let move of possibleMoves) {
+            const newX =x + move[0];
+            const newY = y + move[1];
+            const marker = `${newX}, ${newY}`;
+
+            if (isCoordinateValid(newX, newY) && !passedPoints.has(marker)) {
+                list.push(
+                    {
+                        position: [newX, newY],
+                        direction: [...presentPosition.direction, [newX, newY]]
+                    }
+                );
+                passedPoints.add(marker);
+            }
+        }
+    }
+}
+
+moveKnight([0, 0], [7, 7]);
